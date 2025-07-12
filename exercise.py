23106008d593aab930770fd10b3e4b266fa5c2bf
Iyanu Balogun <iyanubalogun017@gmail.com>
2025-07-12 15:44:44 -0400

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -3,21 +3,32 @@ a = 100
 b = a
 print("Before:", a, b, id(a), id(b))
 
-a += 1
-print("After a += 1:", a, b, id(a), id(b))
+#Output: Before: 100 100 483096446336 483096446336
 
 # Observations:
 # 1. For integers, modifying the value creates a new object, hence the id changes   
+
+a += 1
+print("After a += 1:", a, b, id(a), id(b))
+
+#Output: After a += 1: 101 100 483096446368 483096446336
+
+# Observations:
+#2. For lists, modifying the content does not create a new object, hence the id remains the same
 
 x = [1, 2, 3]
 y = x
 print("Before:", x, y, id(x), id(y))
 
+#Output: Before: [1, 2, 3] [1, 2, 3] 480869360512 480869360512
+
 x.append(4)
 print("After x.append(4):", x, y, id(x), id(y))
 
-# Observations:
-#2. For lists, modifying the content does not create a new object, hence the id remains the same
+#Output: After x.append(4): [1, 2, 3, 4] [1, 2, 3, 4] 480869360512 480869360512
+
+
+
 
 
 #Tasks:
@@ -47,6 +58,8 @@ for n in names:  # iterate through each name in names
 
 print(upper_names)
 
+#Output: ['ALICE', 'BOB', 'CHARLIE', 'DANA']
+
 # Task B: Use enumerate to print index + 1 alongside name
 # Expected output:
 # 1. Alice
@@ -56,18 +69,32 @@ print(upper_names)
 for idx, name in enumerate(upper_names, start=1):
     print(f"{idx}. {name}")
 
+#Output: 
+1. ALICE
+2. BOB
+3. CHARLIE
+4. DANA
+
+
 # Task C: Demonstrate at least two list methods (e.g., insert, pop, remove, sort)
 #   1. Insert a new name at position 2
-
 names.insert(1, "eve")
 print("After insert:", names)
+
+#Output: After insert: ['alice', 'eve', 'bob', 'charlie', 'dana']
+
 #   2. Remove "charlie"
 names.remove("charlie")
 print("After remove:", names)
+
+#Output: After remove: ['alice', 'eve', 'bob', 'dana']
+
 #   3. Sort the list
 names.sort()
 print("After sort:", names)
 # Print the list after each operation.
+
+#Output: After sort: ['alice', 'bob', 'dana', 'eve']
 
 
 #Discuss: What’s the difference between modifying names vs. creating a new list?
